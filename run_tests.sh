ce7a00902e79361331d048ac16009f4c7cc63f79
Ciocanel Razvan <chocksy@gmail.com>
2025-03-21 19:16:32 +0200

    feat: Transition to pdm for python env

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,6 @@
 
 # Add both the project root and tests directory to Python path
 export PYTHONPATH="$(pwd):$(pwd)/Tests:$PYTHONPATH"
-export PIPENV_VERBOSITY=-1
 
 # Define coverage paths as arrays
 INCLUDE_PATHS=(
@@ -35,19 +34,19 @@ OMIT_STRING=$(IFS=,; echo "${OMIT_PATHS[*]}")
 # Run mamba tests
 echo "Starting mamba tests..."
 if [ "$CI" = "true" ]; then
-  pipenv run mamba Tests/specs --enable-coverage --format=junit > junit.xml
+  pdm run mamba Tests/specs --enable-coverage --format=junit > junit.xml
 else
-  pipenv run mamba Tests/specs --enable-coverage
+  pdm run mamba Tests/specs --enable-coverage
 fi
 
 # Generate coverage reports in HTML format
-pipenv run coverage html \
+pdm run coverage html \
   --include="$INCLUDE_STRING" \
   --omit="$OMIT_STRING"
 
 # Generate XML coverage only in CI environment
 if [ "$CI" = "true" ]; then
-  pipenv run coverage xml \
+  pdm run coverage xml \
     --include="$INCLUDE_STRING" \
     --omit="$OMIT_STRING"
 fi
